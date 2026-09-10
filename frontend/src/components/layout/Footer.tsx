b5b3de497b3b9c9925851998701d77a024f161e1
samiul2102013 <samiulhasan0186@gmail.com>
2026-09-10 17:51:54 +0600

    contact info updated

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -34,8 +34,8 @@ export function Footer() {
         <div className="col-span-1">
           <h3 className="text-[16px] font-bold text-black mb-4 font-poppins uppercase tracking-wider">Contact</h3>
           <ul className="space-y-3">
-            <li className="text-[#666666] text-[13px] font-poppins">Email: info@purixiabd.com</li>
-            <li className="text-[#666666] text-[13px] font-poppins">Phone: +880 1923-456666</li>
+            <li className="text-[#666666] text-[13px] font-poppins">Email: purixiabd@gmail.com</li>
+            <li className="text-[#666666] text-[13px] font-poppins">Phone: +880 1334-177275</li>
             <li className="text-[#666666] text-[13px] font-poppins">Address: Dhaka, Bangladesh</li>
           </ul>
         </div>
